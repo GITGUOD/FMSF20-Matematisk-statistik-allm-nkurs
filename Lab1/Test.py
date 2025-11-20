0ce570f1811081301c5928b20aa8017761c01788
Tonny Huynh <tonnyhuynh02@gmail.com>
2025-11-20 18:15:39 +0100

    All exercises and mosquitos done

---
+++ Lab1/Test.py
@@ -0,0 +1,11 @@
+import numpy as np
+import scipy.stats as stats
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+Lambda = 0.49
+u = 0.453
+x = - (1/Lambda) * np.log(1 - u)
+
+print(x)
